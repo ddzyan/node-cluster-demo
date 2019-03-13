3b71424ae8f2cc3e21474fe5594235b193498f18
dingdz <ddzyan@163.com>
2019-03-13 14:24:24 +0800

    添加每个项目说明文档

--- demo0/index.js
+++ demo0/index.js
@@ -1,26 +1,28 @@
-var Promise = require('bluebird');
-var fiboTasks = [44, 42, 42, 43];
+const Promise = require('bluebird');
+const fiboTasks = [44, 42, 42, 43];
 
-function fibo(n) {
+// 斐波那契数列函数
+const fibo = (n) => {
   return n == 0 ? 0 : n > 1 ? fibo(n - 1) + fibo(n - 2) : 1;
-}
+};
 
-function excuteFibo(seq, taskID) {
+const excuteFibo = (seq, taskID) => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      var st = Date.now();
-      var result = fibo(seq);
+      const st = Date.now();
+      const result = fibo(seq);
       console.log(`Task ${taskID} was complete and using ${Date.now() -
     st} ms`);
       resolve(result);
     }, Math.random() * 10);
   });
-}
-var st = Date.now();
+};
+
+const st = Date.now();
 // 异步执行 excuteFibo 任务,是 for + promise.all的集合用法
 Promise.map(fiboTasks, function (item, index) {
   return excuteFibo(item,
     index)
 }).then(function (result) {
   console.log(`All tasks were complete and using ${Date.now() - st} ms`);
-})
+});
